a8fe947a4ab48b661dd3616cabe9b37622c16eb5
MohamedBassem <medoox240@gmail.com>
2014-11-23 23:34:03 +0200

    Adding instruction and parser

--- Instruction.py
+++ Instruction.py
@@ -1,2 +1,20 @@
 class Instruction:
-    pass
+    
+    def __init__(self, type_):
+        self.type_ = type_
+        self.reg_a = -1
+        self.reg_b = -1
+        self.reg_c = -1
+        self.imm = -1
+    
+    def set_reg_a(self, reg_a):
+        self.reg_a = reg_a
+
+    def set_reg_b(self, reg_b):
+        self.reg_b = reg_b
+
+    def set_reg_c(self, reg_c):
+        self.reg_c = reg_c
+
+    def set_imm(self, imm):
+        self.imm = imm
--- InstructionParser.py
+++ InstructionParser.py
@@ -1,2 +1,63 @@
+import re
 class InstructionParser:
-    pass
+    
+    def parse(cls, line):
+        reg_a = -1
+        reg_b = -1
+        reg_c = -1
+        imm = -1
+        type_ = ""
+
+        if re.match("(LW|SW) +([^, ]+) *,? *([^, ]+) *,? *([^, ]+)", line):
+            m = re.match("(LW|SW) +([^, ]+) *,? *([^, ]+) *,? *([^, ]+)", line)
+            type_ = get_type(m.group(1))
+            reg_a = int(m.group(2))
+            reg_b = int(m.group(3))
+            imm = int(m.group(4))
+        elif re.match("(JMP) +([^, ]+) *,? *([^, ]+)",line):
+            m = re.match("(JMP) +([^, ]+) *,? *([^, ]+)",line)
+            type_ = get_type(m.group(1))
+            reg_a = int(m.group(2))
+            imm = int(m.group(3))
+        elif re.match("(BEQ|ADDI) +([^, ]+) *,? *([^, ]+) *,? *([^, ]+)",line):
+            m = re.match("(BEQ|ADDI) +([^, ]+) *,? *([^, ]+) *,? *([^, ]+)",line)
+            type_ = get_type(m.group(1))
+            reg_a = int(m.group(2))
+            reg_b = int(m.group(3))
+            imm = int(m.group(4))
+        elif re.match("(JALR) +([^, ]+) *,? *([^, ]+)",line):
+            m = re.match("(JALR) +([^, ]+) *,? *([^, ]+)",line)
+            type_ = get_type(m.group(1))
+            reg_a = int(m.group(2))
+            reg_b = int(m.group(3))
+        elif re.match("(RET) +([^, ]+)",line):
+            m = re.match("(RET) +([^, ]+)",line)
+            type_ = get_type(m.group(1))
+            reg_a = int(m.group(2))
+        elif re.match("(ADD|SUB|ADDI|NAND) +([^, ]+) *,? *([^, ]+) *,? *([^, ]+)",line):
+            m = re.match("(ADD|SUB|ADDI|NAND) +([^, ]+) *,? *([^, ]+) *,? *([^, ]+)",line)
+            type_ = get_type(m.group(1))
+            reg_a = int(m.group(2))
+            reg_b = int(m.group(3))
+            reg_c = int(m.group(4))
+        inst = Instruction(type_)
+        inst.set_reg_a(reg_a)
+        inst.set_reg_b(reg_b)
+        inst.set_reg_c(reg_c)
+        inst.set_imm(imm)
+        return inst
+
+    def get_type(cls, command):
+        return {
+            "LW": InstructionType.load,
+            "SW": InstructionType.store,
+            "JMP": InstructionType.jump,
+            "BEQ": InstructionType.branch_if_equal,
+            "JALR": InstructionType.jump_and_link,
+            "RET": InstructionType.return_,
+            "ADD": InstructionType.add,
+            "SUB": InstructionType.subtract,
+            "ADDI": InstructionType.add_immediate,
+            "NAND": InstructionType.nand,
+            "MUL": InstructionType.multiply
+        }[command]

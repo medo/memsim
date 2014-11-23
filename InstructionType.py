c55ef9740917ced42fda07383a3e112b11e4c0a8
MohamedBassem <medoox240@gmail.com>
2014-11-23 19:24:42 +0200

    Adding Instruction type enum

--- InstructionType.py
+++ InstructionType.py
@@ -1,3 +1,13 @@
 from enum import Enum
 class InstructionType(Enum):
-    pass
+    load = 1
+    store = 2
+    jump = 3
+    branch_if_equal = 4
+    jump_and_link = 5
+    return_ = 6
+    add = 7
+    subtract = 8
+    add_immediate = 9
+    nand = 10
+    multiply = 11

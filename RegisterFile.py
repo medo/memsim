b2f074596cc6b451cf476dfbf77cddf37c6643f8
MohamedBassem <medoox240@gmail.com>
2014-11-23 17:42:12 +0200

    Adding register file

--- RegisterFile.py
+++ RegisterFile.py
@@ -1,2 +1,10 @@
 class RegisterFile:
-    pass
+    
+    def __init__(self, size):
+        self.data = [0 for i in range(size)]
+
+    def get(self, idx):
+        return self.data[idx]
+
+    def set(self, idx, value):
+        self.data[idx] = value

64c56171cfd2374e1da3d3a6bc7d45880155898a
farghalico <mohamed@farghal.com>
2014-11-24 20:41:26 +0200

    fix

--- memsim/Cache.py
+++ memsim/Cache.py
@@ -88,11 +88,10 @@ class Cache(BaseMemory):
             self.__parent_memory.write_in_address(address, value)
 
 
-        print "Cache miss"
         self.__misses += 1
-        result = self.parent_memory.get_line(address)
+        result = self.__parent_memory.get_line(address)
         self.__cache(address, result[1])
-        return (result[0] + self.__miss_cycles, result[1])
+        return (result[0] + self.__hit_cycles, result[1])
 
 
     def get_misses(self):
